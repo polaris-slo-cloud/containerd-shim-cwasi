8b9da2358c5327058812c9a60a7294fc804c0103
Cynthia Marcelino <keniack@gmail.com>
2023-05-07 18:23:44 +0200

    first experiments

---
+++ examples/func_b_vanilla/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    println!("Greetings from func_b!");
+    cwasi_function();
+}
+
+
+#[no_mangle]
+pub fn cwasi_function() -> i32 {
+
+    let args: Vec<String> = std::env::args().collect();
+    println!("args: {:?}", args);
+
+    println!("do something file");
+    let input:String = args[2].parse().unwrap();
+
+    //process_response(response_string);
+    println!("input {:?}",input);
+    let result:i32 = 5;
+    return result;
+}
+
+pub fn process_response(input_string: &str){
+    println!("Process response ");
+    let full_payload = "{\"source_channel\":\"func_a.wasm\",\"target_channel\":\"func_b.wasm\",\"payload\":\"".to_owned() +input_string+"\"}";
+    let input_bytes = full_payload.as_bytes();
+    let len = input_bytes.len() as i32;
+    let ptr = input_bytes.as_ptr();
+    let ptr_i32 = input_bytes.as_ptr() as i32;
+    println!("input pointer {:?} ",ptr);
+    println!("input length {:?} ",len);
+
+    unsafe {
+        let response_length =cwasi_export::func_connect(ptr_i32,len);
+        println!("res len {:?} ",response_length);
+        let bytes = std::slice::from_raw_parts(ptr, response_length as usize);
+        let response = String::from_utf8_lossy(bytes).to_string();
+        println!("response string {:?} ",response);
+    }
+
+}
+
+pub mod cwasi_export {
+    #[link(wasm_import_module = "cwasi_export")]
+    extern "C" {
+        pub fn func_connect(ptr: i32, len: i32) -> i32;
+    }
+}
